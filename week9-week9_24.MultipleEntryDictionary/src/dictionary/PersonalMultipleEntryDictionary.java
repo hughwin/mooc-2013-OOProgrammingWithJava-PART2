694adc8a929d962cf3100f99f2a47b0cecd0ffed
hughwin <hugh.winchester@gmail.com>
2019-07-13 12:22:48 +0100

    Solution

---
+++ week9-week9_24.MultipleEntryDictionary/src/dictionary/PersonalMultipleEntryDictionary.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dictionary;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author Hugh
+ */
+public class PersonalMultipleEntryDictionary implements MultipleEntryDictionary {
+
+    private HashMap<String, Set<String>> keys;
+    
+    public PersonalMultipleEntryDictionary(){
+        this.keys = new HashMap<String, Set<String>>();
+    }
+    
+    @Override
+    public void add(String word, String entry) {
+        if (!this.keys.containsKey(word)){
+            this.keys.put(word, new HashSet<String>());
+        }
+        
+        Set<String> finished = this.keys.get(word);
+        finished.add(entry);
+    }
+
+    @Override
+    public Set<String> translate(String word) {
+
+            Set<String> a = this.keys.get(word);
+        
+        
+        return a;
+    }
+
+    @Override
+    public void remove(String word) {
+        if (this.keys.containsKey(word)){
+            this.keys.remove(word);
+        }
+    }
+    
+}
