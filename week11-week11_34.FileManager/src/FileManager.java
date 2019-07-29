30e23bbcdecf80b8485fff55aa0fb75e39fe7fdc
hughwin <hugh.winchester@gmail.com>
2019-07-29 11:15:56 +0100

    Solutions

--- week11-week11_34.FileManager/src/FileManager.java
+++ week11-week11_34.FileManager/src/FileManager.java
@@ -10,12 +10,31 @@ import java.util.Scanner;
 public class FileManager {
 
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+ 
+        ArrayList<String> x = new ArrayList<String>();
+        
+                Scanner reader = new Scanner(new File(file));
+        while (reader.hasNextLine()) {
+            String line = reader.nextLine();
+            x.add(line);
+        }
+
+        reader.close();
+        return x;
     }
 
     public void save(String file, String text) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write(text);
+        writer.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+       FileWriter writer = new FileWriter(file);
+        for (String x : texts){
+            writer.write(x);
+            writer.write(System.getProperty("line.separator"));
+        }
+        writer.close();
     }
 }
