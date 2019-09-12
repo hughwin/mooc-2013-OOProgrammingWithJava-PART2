352e053ad8d1e4b8dbde98a6e48c20cceb0be159
hughwin <hugh.winchester@gmail.com>
2019-09-12 20:00:05 +0100

    Solutions

--- week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
+++ week11-week11_36.Greeter/src/greeter/ui/UserInterface.java
@@ -2,6 +2,7 @@ package greeter.ui;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.WindowConstants;
@@ -9,12 +10,31 @@ import javax.swing.WindowConstants;
 public class UserInterface implements Runnable {
 
     private JFrame frame;
+    
+    public UserInterface(){
+    }
 
     @Override
     public void run() {
+        frame = new JFrame("Swing on");
+        frame.setPreferredSize(new Dimension(400, 200));
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        createComponents(frame.getContentPane());
+        
+        frame.pack();
+        frame.setVisible(true);
     }
 
     private void createComponents(Container container) {
+        
+        JButton button = new JButton("Click");
+        container.add(button);
+        
+                JLabel text = new JLabel("Hi!");
+        container.add(text);
+        
     }
 
     public JFrame getFrame() {
--- week11-week11_37.Survey/src/survey/UserInterface.java
+++ week11-week11_37.Survey/src/survey/UserInterface.java
@@ -10,11 +10,50 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
-        // Create your app here
+        frame = new JFrame("Survey");
+        frame.setPreferredSize(new Dimension(400,200));
+        
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        createComponents(frame.getContentPane());
+        
+        frame.pack();
+        frame.setVisible(true);
     }
 
 
     public JFrame getFrame() {
         return frame;
+    }
+
+    private void createComponents(Container container) {
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+        
+        container.add(new JLabel("Are you?"));
+        
+        JCheckBox yes = new JCheckBox("Yes!");
+        JCheckBox no = new JCheckBox("no!");
+        
+        container.add(yes);
+        container.add(no);
+        
+        container.add(new JLabel("Why?"));
+        
+        JRadioButton noReason = new JRadioButton("No reason!");
+        JRadioButton fun = new JRadioButton("Because it is fun!");
+        
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(noReason);
+        buttonGroup.add(fun);
+        
+        container.add(noReason);
+        container.add(fun);
+        
+        JButton done = new JButton("Done!");
+        
+        container.add(done);
+        
+        
     }
 }
