509ea5625e4ce2f8490bc2c9c7a70b8046267b56
hughwin <hugh.winchester@gmail.com>
2019-09-14 11:20:30 +0100

    Solutions

---
+++ week11-week11_39.ClickEffect/src/clicker/applicationlogic/PersonalCalculator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clicker.applicationlogic;
+
+public class PersonalCalculator implements  Calculator {
+    
+    private int count;
+    
+    public PersonalCalculator(){
+        this.count = 0;
+    }
+
+    @Override
+    public int giveValue() {
+        return this.count;
+    }
+    
+
+    @Override
+    public void increase() {
+        this.count++;
+    }
+    
+    
+    
+}
--- week11-week11_39.ClickEffect/src/clicker/ui/UserInterface.java
+++ week11-week11_39.ClickEffect/src/clicker/ui/UserInterface.java
@@ -1,13 +1,20 @@
 package clicker.ui;
 
+import clicker.applicationlogic.Calculator;
+import clicker.applicationlogic.PersonalCalculator;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import javax.swing.*;
 
 public class UserInterface implements Runnable {
     private JFrame frame;
+    private Calculator calculator;
 
-
+    UserInterface(Calculator calculator){
+        this.calculator = new PersonalCalculator();
+    }
+    
     @Override
     public void run() {
         frame = new JFrame("Click Effect");
@@ -21,6 +28,22 @@ public class UserInterface implements Runnable {
     }
 
     private void createComponents(Container container) {
+        GridLayout layout = new GridLayout(2, 1);
+        container.setLayout(layout);
+        
+        JLabel count = new JLabel("0");
+        
+        JButton increment = new JButton("Click!");
+        ClickListener listener = new ClickListener(this.calculator, count);
+        
+        increment.addActionListener(listener);
+        
+        container.add(count);
+        container.add(increment);
+        
+        
+        
+        
     }
 
     public JFrame getFrame() {
